8d494883235fcdfdd640c878652808192ed4aadc
Monochromish <79590499+Monochromish@users.noreply.github.com>
2021-08-15 01:18:52 +0530

    Update triggered.js

--- commands/Memes/triggered.js
+++ commands/Memes/triggered.js
@@ -1,17 +1,14 @@
 const canvacord = require('canvacord');
 const Canvacord = require('canvacord/src/Canvacord');
 const {  MessageAttachment } = require('discord.js');
-
 module.exports = {
     name: 'triggered',
     description: "Trigger Yourself",
     run: async(client, message, args) => {
-        let avatar = message.author.displayAvatarURL({dynamic: false, format: "png"})
-
+        let target = message.mentions.members.first() ||message.mentions.members.last() || message.member;
+        let avatar = target.displayAvatarURL({dynamic: false, format: "png"})
         let image = await Canvacord.trigger(avatar)
-
         let triggered = new MessageAttachment(image, "triggered.gif")
-
         message.channel.send(triggered)
     }
 }
