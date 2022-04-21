67a61da7b56e48e4530cbfd596435111449e2c9a
Monochromish <79590499+Monochromish@users.noreply.github.com>
2022-04-22 06:56:41 +1000

    Fixed permission barrier

--- src/Events/InteractionCreate.js
+++ src/Events/InteractionCreate.js
@@ -7,7 +7,7 @@ module.exports = {
     const command = bot.commands.get(interaction.commandName);
     if (!command) return;
 
-    if (command.permission && !interaction.member.permissions.has(command.permission))
+    if (command.permission && !interaction.member.permissions.has(command.permission)) {
       return await interaction.reply({
         embeds: [
           new MessageEmbed()
@@ -15,27 +15,28 @@ module.exports = {
             .setDescription(`You require the \`${command.permission}\` to run this command.`)
         ]
       });
-
-    // If command's category is `NSFW` and if interaction.channel.nsfw is false, inform them to use the command in nsfw enabled channel.
-    if (command.category === 'NSFW' && !interaction.channel.nsfw) {
-      await interaction[interaction.deferred ? 'editReply' : interaction.replied ? 'followUp' : 'reply']({
-        embeds: [
-          new MessageEmbed()
-            .setColor('RED')
-            .setDescription('You can use this command in Age-Restricted/NSFW enabled channels only.')
-            .setImage('https://i.imgur.com/oe4iK5i.gif')
-        ],
-        ephemeral: true
-      });
     } else {
-      try {
-        await command.run({ interaction, bot, options: interaction.options, guild: interaction.guild });
-      } catch (err) {
-        console.log(err);
-
+      // If command's category is `NSFW` and if interaction.channel.nsfw is false, inform them to use the command in nsfw enabled channel.
+      if (command.category === 'NSFW' && !interaction.channel.nsfw) {
         await interaction[interaction.deferred ? 'editReply' : interaction.replied ? 'followUp' : 'reply']({
-          embeds: [new MessageEmbed().setColor('RED').setDescription(err.message || 'Unexpected error')]
+          embeds: [
+            new MessageEmbed()
+              .setColor('RED')
+              .setDescription('You can use this command in Age-Restricted/NSFW enabled channels only.')
+              .setImage('https://i.imgur.com/oe4iK5i.gif')
+          ],
+          ephemeral: true
         });
+      } else {
+        try {
+          await command.run({ interaction, bot, options: interaction.options, guild: interaction.guild });
+        } catch (err) {
+          console.log(err);
+
+          await interaction[interaction.deferred ? 'editReply' : interaction.replied ? 'followUp' : 'reply']({
+            embeds: [new MessageEmbed().setColor('RED').setDescription(err.message || 'Unexpected error')]
+          });
+        }
       }
     }
   }
